cb0c8e7945aa6426dc0b2f89ba7da9025deb2698
slybixi <85377203+slybixi@users.noreply.github.com>
2022-02-21 00:11:48 +0100

    Update views.py

--- pwa/views.py
+++ pwa/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 
 from pwa.defaults import get_app, get_pwa_config, get_service_worker
 
-# Create your views here.
+from teller_app.models import Index
+from teller_app.models import Airdrop
+from teller_app.models import Meta
+from teller_app.models import Airdrop_Meta
 
+# Create your views here.
 
 def manifest_json(request):
 	return JsonResponse(get_pwa_config())
@@ -17,9 +21,16 @@ def sw_js(request):
 def app_js(request):
 	return HttpResponse(get_app() , content_type='application/javascript')
 	
-
 def offline(request):
-	try:
-		return render(request, 'pwa/offline.html')
-	except:
-		return render(request, 'pwa/pwa_offline.html')
+    home = Index.objects.all()
+    meta = Meta.objects.all()
+    airdrop = Airdrop.objects.all()
+    airdrop_meta = Airdrop_Meta.objects.all()
+    context = {
+        'home': home,
+        'meta': meta,
+        'airdrop': airdrop,
+        'airdrop_meta': airdrop_meta
+    }
+    
+    return render(request, 'pwa/pwa_offline.html', context)
